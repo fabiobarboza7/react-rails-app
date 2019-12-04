a84c12d375ceec91b8e78038b3eb8ec56923f2a3
Fábio Souza <fabiobarboza7@hotmail.com>
2019-12-04 02:00:58 -0300

    starting new react-rails-app

---
+++ app/javascript/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Home from './components/Home'
+// import { Container } from './styles';
+
+export default function javascript() {
+  return (
+    <Home />
+  );
+}
